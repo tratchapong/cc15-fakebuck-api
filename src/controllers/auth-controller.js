8d644e1d6b39d08674cffd05ddc58c6b53c62485
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-10-02 15:10:35 +0700

    update register response

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -22,7 +22,8 @@ exports.register = async (req, res, next) => {
         expiresIn: process.env.JWT_EXPIRE
       }
     );
-    res.status(201).json({ accessToken });
+    delete user.password;
+    res.status(201).json({ accessToken, user });
   } catch (err) {
     next(err);
   }
