53a39cfe29fbae3d3a89b68a436212ca1757d1b4
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2023-10-09 13:57:11 +0700

    update feateure create post

--- src/controllers/post-controller.js
+++ src/controllers/post-controller.js
@@ -33,10 +33,25 @@ exports.createPost = async (req, res, next) => {
       data.message = message;
     }
 
-    await prisma.post.create({
-      data: data
+    const post = await prisma.post.create({
+      data: data,
+      include: {
+        user: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            profileImage: true
+          }
+        },
+        likes: {
+          select: {
+            userId: true
+          }
+        }
+      }
     });
-    res.status(201).json({ message: 'post created' });
+    res.status(201).json({ message: 'post created', post });
   } catch (err) {
     next(err);
   } finally {
